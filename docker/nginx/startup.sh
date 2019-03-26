410eaf77180e5f3d8e6c703900bbd57bd2bc44d3
hajime1103 <mh52124@gmail.com>
2019-03-26 22:50:48 +0900

    CDの実装ファイルの追加

---
+++ docker/nginx/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f /etc/nginx/ssl/default.crt ]; then
+    openssl genrsa -out "/etc/nginx/ssl/default.key" 2048
+    openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=default/O=default/C=UK"
+    openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
+fi
+
+# Nuxt server lunch
+npm run build
+forever start -c "npm run start" ./
+
+# nginx sever lunch
+nginx
