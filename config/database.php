72334fc4f02f4911ba3db15499c334c1f583b72a
hajime1103 <mh52124@gmail.com>
2019-04-02 00:21:55 +0900

    database.php　DB名を変更

--- config/database.php
+++ config/database.php
@@ -43,16 +43,17 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             //'host' => env('DB_HOST', '127.0.0.1'),
-            // 'port' => env('DB_PORT', '3306'),
+            //'port' => env('DB_PORT', '3306'),
             'read' => [
                 'host' => env('DB_SLAVE_HOST', '127.0.0.1'),
                 'port' => env('DB_SLAVE_PORT', '3306'),
+                'database' => env('DB_SLAVE_DATABASE', 'forge'),
             ],
             'write' => [
                 'host' => env('DB_MASTER_HOST', '127.0.0.1'),
                 'port' => env('DB_MASTER_PORT', '3306'),
+                'database' => env('DB_MASTER_DATABASE', 'forge'),
             ],
-            'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
